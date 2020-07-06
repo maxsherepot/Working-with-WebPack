a47e78a9a938c6fa6f1f30248ebf5cf59dc5b9f9
maxsherepot <48758609+maxsherepot@users.noreply.github.com>
2020-07-06 18:14:11 +0300

    WebPack installation

---
+++ build/main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => /*#__PURE__*/React.createElement("p", null, "Hello");
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.GetElementById("root"));
