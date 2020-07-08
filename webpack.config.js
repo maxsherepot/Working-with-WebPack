c58d3920dc3a0bde9e20c52ddc66b3b0be80e3b3
maxsherepot <48758609+maxsherepot@users.noreply.github.com>
2020-07-08 16:05:55 +0300

    Production configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,71 +2,96 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    mode: "development",
+module.exports = (env = {}) => {
 
-    module: {
-        rules: [
+    const { mode = "development" } = env;
 
-            ////////////// css loaders
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            },
+    const isProd = mode == "production";
+    const isDev = mode == "development";
 
-            ////////////// sass/scss loaders
-            {
-                test: /\.(s[ca]ss)$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-            },
+    const getStyleLoaders = () => {
+        return [isProd ? MiniCssExtractPlugin.loader : "style-loader", "css-loader"];
+    };
 
-            ////////////// babel loader 
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            },
+    const getPlugins = () => {
+        const plugins = [
+            new HtmlWebpackPlugin({
+                template: "public/index.html"
+            }),
+        ];
+        if (isProd) {
+            plugins.push(new MiniCssExtractPlugin({
+                filename: "main-[hash:8].css"
+            }))
+        }
 
-            ////////////// images loader
-            {
-                test: /\.(png|jpg|jpeg|gif|ico)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: "images",
-                            name: "[name]-[sha1:hash:7].[ext]"
+        return plugins;
+    };
+
+    return {
+        mode: isProd ? "production" : isDev && "development",
+
+        output: {
+            filename: isProd ? "main-[hash:8].js" : undefined
+        },
+
+        module: {
+            rules: [
+
+                ////////////// css loaders
+                {
+                    test: /\.css$/,
+                    use: getStyleLoaders()
+                },
+
+                ////////////// sass/scss loaders
+                {
+                    test: /\.(s[ca]ss)$/,
+                    use: [...getStyleLoaders(), "sass-loader"]
+                },
+
+                ////////////// babel loader 
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: "babel-loader"
+                },
+
+                ////////////// images loader
+                {
+                    test: /\.(png|jpg|jpeg|gif|ico)$/,
+                    use: [
+                        {
+                            loader: "file-loader",
+                            options: {
+                                outputPath: "images",
+                                name: "[name]-[sha1:hash:7].[ext]"
+                            }
                         }
-                    }
-                ]
-            },
+                    ]
+                },
 
-            ////////////// fonts loader
-            {
-                test: /\.(ttf|otf|eot|woff|woff2)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: "fonts",
-                            name: "[name].[ext]"
+                ////////////// fonts loader
+                {
+                    test: /\.(ttf|otf|eot|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: "file-loader",
+                            options: {
+                                outputPath: "fonts",
+                                name: "[name].[ext]"
+                            }
                         }
-                    }
-                ]
-            }
-        ]
-    },
+                    ]
+                }
+            ]
+        },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "public/index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: "main-[hash:8].css"
-        })
-    ],
+        plugins: getPlugins(),
 
-    devServer: {
-        open: true
-    }
+        devServer: {
+            open: true
+            // noInfo: true
+        }
+    };
 };
